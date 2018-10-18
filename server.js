c3d7bf466c61a1c82baa6e5fc8762b73a7cb6878
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-10-18 15:06:29 +0100

    tables in SQL has been created and post(/api/createorder is working

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require('express');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+const express = require('express');
 const app = express();
+const db = pgp({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+});
 
 app.use(bodyParser.json());
 app.use('/static', express.static('static'));
@@ -14,10 +24,22 @@ const menu = {
   }
 };
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(8080, function(){
+app.post('/api/createorder', (req, res) => {
+
+  db.one(`INSERT INTO orders (id) VALUES (default) RETURNING id`)
+    .then((data) => {
+      res.json({ id: data.id })
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({ error: error.message });
+    })
+})
+
+app.listen(8080, function () {
   console.log('Listening on port 8080');
 });
